adde38d12217fc4c327c80f3141efd53049994c2
Kenny <kenny26plis@gmail.com>
2026-03-01 11:10:18 -0300

    SH1107: Defining state variables and creating new error codes

--- SH1107_Library_for_STM32G0B1/Inc/SH1107_Library_STM32.h
+++ SH1107_Library_for_STM32G0B1/Inc/SH1107_Library_STM32.h
@@ -10,8 +10,8 @@
 
 #include "stm32g0xx_hal.h"
 
-#define SH1107_MAX_MULTIPLEX 127
-#define SH1107_128x128_MULTIPLEX 127
+#define SH1107_MAX_MULTIPLEX 	 0x7FU
+#define SH1107_128x128_MULTIPLEX 0x7FU
 
 #define SH1107_WIDTH  128
 #define SH1107_HEIGHT 128
@@ -25,23 +25,21 @@ typedef struct
 	uint16_t pin;
 }SH1107_GPIO_t;
 
-typedef struct
-{
-	SPI_HandleTypeDef *hspi;
-
-	SH1107_GPIO_t cs_pin;
-	SH1107_GPIO_t dc_pin;
-	SH1107_GPIO_t reset_pin;
-
-	uint8_t buffer[SH1107_PAGES][SH1107_WIDTH];
-}SH1107_HandleTypeDef;
-
 typedef enum{
 	SH1107_OK                    		= 0,
-	SH1107_ERROR_SPI_HANDLE_NOT_DEFINED = 1,
-	SH1107_ERROR_PIN_NOT_DEFINED 		= 2,
-	SH1107_ERROR_INCORRECT_PARAMETER    = 3
+	SH1107_ERROR_SH1107_NOT_DEFINED     = 1,
+	SH1107_ERROR_SPI_HANDLE_NOT_DEFINED = 2,
+	SH1107_ERROR_SPI_CS_NOT_DEFINED     = 3,
+	SH1107_ERROR_SPI_DC_NOT_DEFINED     = 4,
+	SH1107_ERROR_PIN_NOT_DEFINED 		= 5,
+	SH1107_ERROR_SPI_HAL                = 6,
+	SH1107_ERROR_INCORRECT_PARAMETER    = 7
 }SH1107_ERROR;
+
+typedef enum{
+	SH1107_SPI_DISCONNECTED = 0,
+	SH1107_SPI_CONNECTED    = 1
+}SH1107_SPI_Connection;
 
 typedef enum{
 	SH1107_DC_COMMAND = 0,
@@ -59,15 +57,31 @@ typedef enum{
 }SH1107_ADRESSING_MODE;
 
 typedef enum{
-	SH1107_CONTRAST_LOW     = 0x00, //0
-	SH1107_CONTRAST_DEFAULT = 0x80, //128 - max recomended contrast
-	SH1107_CONTRAST_HIGH    = 0xFF
+	SH1107_CONTRAST_LOW     = 0x00U, //0
+	SH1107_CONTRAST_DEFAULT = 0x80U, //128 - max recomended contrast
+	SH1107_CONTRAST_HIGH    = 0xFFU
 }SH1107_CONTRAST;
 
 typedef enum{
 	SH1107_MODE_NORMAL  = 0,
 	SH1107_MODE_INVERSE = 1
 }SH1107_DisplayMode;
+
+typedef struct
+{
+	SPI_HandleTypeDef *hspi;
+
+	SH1107_GPIO_t cs_pin;
+	SH1107_GPIO_t dc_pin;
+	SH1107_GPIO_t reset_pin;
+
+	uint8_t buffer[SH1107_PAGES][SH1107_WIDTH];
+
+	SH1107_Status display_status;
+	SH1107_ADRESSING_MODE adressing_mode;
+	SH1107_CONTRAST contrast;
+	SH1107_DisplayMode display_mode;
+}SH1107_HandleTypeDef;
 
 SH1107_ERROR SH1107_SetSPI(SH1107_HandleTypeDef *sh1107, SPI_HandleTypeDef *hspi);
 SH1107_ERROR SH1107_SetPin_Cs(SH1107_HandleTypeDef *sh1107,GPIO_TypeDef *port, uint16_t pin);
--- SH1107_Library_for_STM32G0B1/Src/SH1107_Library_STM32.c
+++ SH1107_Library_for_STM32G0B1/Src/SH1107_Library_STM32.c
@@ -9,7 +9,6 @@
 #include "SH1107_Library_STM32.h"
 #include "string.h"
 
-//uint8_t sh1107_buffer[SH1107_PAGES][SH1107_WIDTH];
 
 uint8_t convertLineToPage(uint8_t target_line)
 {
@@ -23,7 +22,6 @@ uint8_t convertLineToHex(uint8_t target_line)
 	return bit_hex;
 }
 
-
 /**
  * @brief Receive SPI Handle
  *
@@ -34,6 +32,9 @@ uint8_t convertLineToHex(uint8_t target_line)
  */
 SH1107_ERROR SH1107_SetSPI(SH1107_HandleTypeDef *sh1107, SPI_HandleTypeDef *hspi)
 {
+	if(hspi == NULL)
+		return SH1107_ERROR_SPI_HANDLE_NOT_DEFINED;
+
 	sh1107->hspi = hspi;
 
 	return SH1107_OK;
@@ -50,8 +51,10 @@ SH1107_ERROR SH1107_SetSPI(SH1107_HandleTypeDef *sh1107, SPI_HandleTypeDef *hspi
  */
 SH1107_ERROR SH1107_SetPin_Cs(SH1107_HandleTypeDef *sh1107, GPIO_TypeDef *port, uint16_t pin)
 {
-	if(sh1107->hspi == NULL)
-		return SH1107_ERROR_SPI_HANDLE_NOT_DEFINED;
+	if(sh1107 == NULL)
+		return SH1107_ERROR_SH1107_NOT_DEFINED;
+	if(port == NULL)
+		return SH1107_ERROR_INCORRECT_PARAMETER;
 
 	sh1107->cs_pin.port = port;
 	sh1107->cs_pin.pin  = pin;
@@ -70,8 +73,10 @@ SH1107_ERROR SH1107_SetPin_Cs(SH1107_HandleTypeDef *sh1107, GPIO_TypeDef *port, 
  */
 SH1107_ERROR SH1107_SetPin_Dc(SH1107_HandleTypeDef *sh1107, GPIO_TypeDef *port, uint16_t pin)
 {
-	if(sh1107->hspi == NULL)
-		return SH1107_ERROR_SPI_HANDLE_NOT_DEFINED;
+	if(sh1107 == NULL)
+		return SH1107_ERROR_SH1107_NOT_DEFINED;
+	if(port == NULL)
+		return SH1107_ERROR_INCORRECT_PARAMETER;
 
 	sh1107->dc_pin.port = port;
 	sh1107->dc_pin.pin  = pin;
@@ -90,8 +95,10 @@ SH1107_ERROR SH1107_SetPin_Dc(SH1107_HandleTypeDef *sh1107, GPIO_TypeDef *port, 
  */
 SH1107_ERROR SH1107_SetPin_Reset(SH1107_HandleTypeDef *sh1107, GPIO_TypeDef *port, uint16_t pin)
 {
-	if(sh1107->hspi == NULL)
-		return SH1107_ERROR_SPI_HANDLE_NOT_DEFINED;
+	if(sh1107 == NULL)
+		return SH1107_ERROR_SH1107_NOT_DEFINED;
+	if(port == NULL)
+		return SH1107_ERROR_INCORRECT_PARAMETER;
 
 	sh1107->reset_pin.port  = port;
 	sh1107->reset_pin.pin   = pin;
@@ -114,16 +121,24 @@ SH1107_ERROR SH1107_SetPin_Reset(SH1107_HandleTypeDef *sh1107, GPIO_TypeDef *por
  */
 SH1107_ERROR SH1107_Transmit(SH1107_HandleTypeDef *sh1107, SH1107_DC dc, uint8_t *buffer, uint16_t size)
 {
-	if(sh1107->cs_pin.port != NULL && sh1107->dc_pin.port != NULL)
-	{
-		HAL_GPIO_WritePin(sh1107->cs_pin.port, sh1107->cs_pin.pin, 0);		//seleciona o chip
-		HAL_GPIO_WritePin(sh1107->dc_pin.port, sh1107->dc_pin.pin, dc);		//Enviar um comando
-		HAL_SPI_Transmit(sh1107->hspi, buffer, size, 10);
-		HAL_GPIO_WritePin(sh1107->cs_pin.port, sh1107->cs_pin.pin, 1);
+	HAL_StatusTypeDef errorcode = HAL_OK;
 
+	if(sh1107->hspi == NULL)
+		return SH1107_ERROR_SPI_HANDLE_NOT_DEFINED;
+	if(sh1107->cs_pin.port == NULL)
+		return SH1107_ERROR_SPI_CS_NOT_DEFINED;
+	if(sh1107->dc_pin.port == NULL)
+		return SH1107_ERROR_SPI_DC_NOT_DEFINED;
+
+	HAL_GPIO_WritePin(sh1107->cs_pin.port, sh1107->cs_pin.pin, 0);		//seleciona o chip
+	HAL_GPIO_WritePin(sh1107->dc_pin.port, sh1107->dc_pin.pin, dc);		//Enviar um comando
+	errorcode = HAL_SPI_Transmit(sh1107->hspi, buffer, size, 10);
+	HAL_GPIO_WritePin(sh1107->cs_pin.port, sh1107->cs_pin.pin, 1);
+
+	if(errorcode == HAL_OK)
 		return SH1107_OK;
-	}
-	return SH1107_ERROR_PIN_NOT_DEFINED;
+
+	return SH1107_ERROR_SPI_HAL;
 }
 
 /**
@@ -173,7 +188,12 @@ SH1107_ERROR SH1107_CMD_Display_ON(SH1107_HandleTypeDef *sh1107, SH1107_Status d
 	else
 		return SH1107_ERROR_INCORRECT_PARAMETER;
 
-	return SH1107_Transmit(sh1107, 0, &cmd, 1);
+	SH1107_ERROR error_code = SH1107_Transmit(sh1107, 0, &cmd, 1);
+
+	if(error_code == SH1107_OK)
+		sh1107->display_status = display_on_off;
+
+	return error_code;
 }
 
 SH1107_ERROR SH1107_CMD_SetMultiplex(SH1107_HandleTypeDef *sh1107, uint8_t multiplex)
@@ -195,15 +215,27 @@ SH1107_ERROR SH1107_CMD_SetAdressingMode(SH1107_HandleTypeDef *sh1107, SH1107_AD
 		mode = page_adressing_mode;
 	else if(adress_mode == SH1107_ADRESSING_MODE_VERTICAL)
 		mode = vertical_adressing_mode;
+	else
+		return SH1107_ERROR_INCORRECT_PARAMETER;
 
-	return SH1107_Transmit(sh1107, SH1107_DC_COMMAND, &mode, 1);
+	SH1107_ERROR error_code = SH1107_Transmit(sh1107, SH1107_DC_COMMAND, &mode, 1);
+
+	if(error_code == SH1107_OK)
+		sh1107->adressing_mode = adress_mode;
+
+	return error_code;
 }
 
 SH1107_ERROR SH1107_CMD_SetContrast(SH1107_HandleTypeDef *sh1107, uint8_t contrast)
 {
 	uint8_t cmd_contrast[2] = {0x81, contrast};
 
-	return SH1107_Transmit(sh1107, SH1107_DC_COMMAND, cmd_contrast, sizeof(cmd_contrast));
+	SH1107_ERROR error_code = SH1107_Transmit(sh1107, SH1107_DC_COMMAND, cmd_contrast, sizeof(cmd_contrast));
+
+	if(error_code == SH1107_OK)
+		sh1107->contrast = contrast;
+
+	return error_code;
 }
 
 SH1107_ERROR SH1107_CMD_SetDisplayMode(SH1107_HandleTypeDef *sh1107, SH1107_DisplayMode mode)
@@ -216,12 +248,17 @@ SH1107_ERROR SH1107_CMD_SetDisplayMode(SH1107_HandleTypeDef *sh1107, SH1107_Disp
 	else
 		return SH1107_ERROR_INCORRECT_PARAMETER;
 
-	return SH1107_Transmit(sh1107, SH1107_DC_COMMAND, &cmd_mode, 1);
+	SH1107_ERROR error_code = SH1107_Transmit(sh1107, SH1107_DC_COMMAND, &cmd_mode, 1);
+
+	if(error_code == SH1107_OK)
+		sh1107->display_mode = mode;
+
+	return error_code;
 }
 
 SH1107_ERROR SH1107_CMD_SetColumn(SH1107_HandleTypeDef *sh1107, uint8_t column)
 {
-	if(column > 127)
+	if(column >= SH1107_WIDTH)
 		return SH1107_ERROR_INCORRECT_PARAMETER;
 
 	uint8_t high_bits = 0x10 | ((column >> 4) & 0x07);
@@ -234,7 +271,7 @@ SH1107_ERROR SH1107_CMD_SetColumn(SH1107_HandleTypeDef *sh1107, uint8_t column)
 
 SH1107_ERROR SH1107_CMD_SetPage(SH1107_HandleTypeDef *sh1107, uint8_t page_adress)
 {
-	if(page_adress > 16)
+	if(page_adress >= SH1107_PAGES)
 		return SH1107_ERROR_INCORRECT_PARAMETER;
 
 	uint8_t cmd_page_adress = 0xB0 | (page_adress & 0x0F);
@@ -244,6 +281,9 @@ SH1107_ERROR SH1107_CMD_SetPage(SH1107_HandleTypeDef *sh1107, uint8_t page_adres
 
 SH1107_ERROR SH1107_CMD_SetCursor(SH1107_HandleTypeDef *sh1107, uint8_t column, uint8_t page_adress)
 {
+	if(column >= SH1107_WIDTH || page_adress >= SH1107_PAGES)
+		return SH1107_ERROR_INCORRECT_PARAMETER;
+
 	SH1107_CMD_SetColumn(sh1107, column);
 	SH1107_CMD_SetPage(sh1107, page_adress);
 
@@ -252,14 +292,17 @@ SH1107_ERROR SH1107_CMD_SetCursor(SH1107_HandleTypeDef *sh1107, uint8_t column, 
 
 SH1107_ERROR SH1107_CMD_WriteDisplayData(SH1107_HandleTypeDef *sh1107, uint8_t *data, uint16_t size)
 {
-	SH1107_Transmit(sh1107, SH1107_DC_DATA, data, size);
+	if(data == NULL)
+		return SH1107_ERROR_INCORRECT_PARAMETER;
 
-	return SH1107_OK;
-
+	return SH1107_Transmit(sh1107, SH1107_DC_DATA, data, size);;
 }
 
 SH1107_ERROR SH1107_DRAW_Page(SH1107_HandleTypeDef *sh1107, uint8_t page, uint8_t *data)
 {
+	if(data == NULL)
+		return SH1107_ERROR_INCORRECT_PARAMETER;
+
 	memcpy(sh1107->buffer[page], data, SH1107_WIDTH);
 
 	return SH1107_OK;
@@ -267,10 +310,10 @@ SH1107_ERROR SH1107_DRAW_Page(SH1107_HandleTypeDef *sh1107, uint8_t page, uint8_
 
 SH1107_ERROR SH1107_CMD_ClearDisplay(SH1107_HandleTypeDef *sh1107)
 {
-	uint8_t clearn_byte[128] = {0x00};
+	uint8_t clearn_page[SH1107_WIDTH] = {0x00};
 
-	for(uint8_t i=0; i<=15; i++)
-		SH1107_DRAW_Page(sh1107, i, clearn_byte);
+	for(uint8_t i=0; i<SH1107_PAGES; i++)
+		SH1107_DRAW_Page(sh1107, i, clearn_page);
 
 	SH1107_Update_Display(sh1107);
 	return SH1107_OK;
@@ -278,6 +321,9 @@ SH1107_ERROR SH1107_CMD_ClearDisplay(SH1107_HandleTypeDef *sh1107)
 
 SH1107_ERROR SH1107_Draw_Pixel(SH1107_HandleTypeDef *sh1107, uint8_t x, uint8_t y)
 {
+	if(x >= SH1107_WIDTH || y >= SH1107_HEIGHT)
+		return SH1107_ERROR_INCORRECT_PARAMETER;
+
 	uint8_t page = convertLineToPage(y);
 	uint8_t byte_column = convertLineToHex(y);
 
@@ -288,6 +334,9 @@ SH1107_ERROR SH1107_Draw_Pixel(SH1107_HandleTypeDef *sh1107, uint8_t x, uint8_t 
 
 SH1107_ERROR SH1107_Update_Page(SH1107_HandleTypeDef *sh1107, uint8_t page)
 {
+	if(page >= SH1107_PAGES)
+		return SH1107_ERROR_INCORRECT_PARAMETER;
+
 	uint8_t *data = sh1107->buffer[page];
 
 	SH1107_CMD_SetCursor(sh1107, 0, page);
@@ -300,19 +349,13 @@ SH1107_ERROR SH1107_Update_Page(SH1107_HandleTypeDef *sh1107, uint8_t page)
 SH1107_ERROR SH1107_Update_Display(SH1107_HandleTypeDef *sh1107)
 {
 	uint8_t page = 0;
-	while(page <= 15)
+	while(page < SH1107_PAGES)
 	{
 		SH1107_Update_Page(sh1107, page);
 		page++;
 	}
 	return SH1107_OK;
 }
-
-
-
-
-
-
 
 
 
